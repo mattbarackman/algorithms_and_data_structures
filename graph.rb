9522cbe41239d8fe04057e60687cb296d9613cff
mattbarackman <matthew.barackman@gmail.com>
2013-09-29 18:10:30 -0400

    added DFS and refactored explored into an instance variable

--- graph.rb
+++ graph.rb
@@ -25,44 +25,75 @@ end
 class Graph
   def initialize(nodes)
     @nodes = []
+    @explored = Hash.new(false)
   end
   def add_node(node)
     @nodes << node
   end
-  def BFS_search(node)
-    explored = Hash.new(false)
+
+  def BFS_search(node, target_node)
     to_explore = [node]
     until to_explore.empty?
       current_node = to_explore.shift
-      explored[current_node] = true
-      p current_node.value
+      
+      if current_node == target_node
+        @explored = Hash.new(false)
+        return true 
+      end
+
+      @explored[current_node] = true
       current_node.connected_nodes.each do |connected_node|
-        unless explored[connected_node] || to_explore.include?(connected_node)
+        unless @explored[connected_node] || to_explore.include?(connected_node)
           to_explore << connected_node
         end
       end
     end
+    @explored = Hash.new(false)
+    false
   end
 
+  def DFS_search(node, target_node)
+    to_explore = [node]
+    until to_explore.empty?
+      current_node = to_explore.pop
+      if current_node == target_node
+        @explored = Hash.new(false)
+        return true 
+      end
+
+      @explored[current_node] = true
+      current_node.connected_nodes.each do |connected_node|
+        unless @explored[connected_node] || to_explore.include?(connected_node)
+          to_explore << connected_node
+        end
+      end
+    end
+    @explored = Hash.new(false)
+    false
+  end
   # this shortest_path algorithm is implemented using a breadth first search algorithm 
   # that additionally tracks the current 'layer' of search a node is reached on, 
   # which is equivalent to the number of hops necessary to get there.
   
   def shortest_path(node, target_node)
     node.layer = 0
-    explored = Hash.new(false)
     to_explore = [node]
     until to_explore.empty?
       current_node = to_explore.shift
-      return current_node.layer if current_node == target_node
-      explored[current_node] = true
+      if current_node == target_node
+        @explored = Hash.new(false)
+        return current_node.layer 
+      end
+      @explored[current_node] = true
       current_node.connected_nodes.each do |connected_node|
-        unless explored[connected_node] || to_explore.include?(connected_node)
+        unless @explored[connected_node] || to_explore.include?(connected_node)
           to_explore << connected_node
           connected_node.layer = current_node.layer + 1
         end
       end
     end
+    Hash.new(false)
+    nil
   end
 end
 
@@ -89,9 +120,18 @@ d.add_connection(e)
 e.add_connection(c)
 e.add_connection(d)
 
-graph = Graph.new([s,a,b,c,d,e])
+f = Node.new("f") # not connected to anything in graph
 
-# p graph.BFS_search(s)
+graph = Graph.new([s,a,b,c,d,e,f])
+
+
+p graph.BFS_search(s, e) == true
+p graph.BFS_search(s, s) == true
+p graph.BFS_search(s, f) == false
+
+p graph.DFS_search(s, e) == true
+p graph.DFS_search(s, s) == true
+p graph.DFS_search(s, f) == false
 
 p graph.shortest_path(s, s) == 0
 p graph.shortest_path(s, a) == 1
@@ -99,3 +139,4 @@ p graph.shortest_path(s, b) == 1
 p graph.shortest_path(s, c) == 2
 p graph.shortest_path(s, d) == 2
 p graph.shortest_path(s, e) == 3
+p graph.shortest_path(s, f) == nil
